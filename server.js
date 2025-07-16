ff25a3ab69566a607940f81e16bd3eb0db067c36
Farkhat Baterikov <farkhat.baterikov@Farkhats-MacBook-Pro.local>
2025-07-16 14:44:00 +0500

    revert:server.js add:debug

--- server.js
+++ server.js
@@ -12,6 +12,5 @@ app.get('/', (req, res) => {
   res.send('Hello World');
 });
 
-app.listen(PORT, HOST, () => {
-  console.log(`Running on http://${HOST}:${PORT}`);
-});
+app.listen(PORT, HOST);
+console.log(`Running on http://${HOST}:${PORT}`);
